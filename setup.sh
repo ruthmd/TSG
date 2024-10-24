bd7a48dc624430b6787d463268fffd34608917fc
Henry <hps1945@live.co.uk>
2024-10-24 19:20:49 +0100

    UPDATE: submittable on Andrena

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ cd TSG
 
 echo "Setting up python environment"
 python3 -m venv .venv
+source .venv/bin/activate
 python3 -m pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
 python3 -m pip install tqdm gensim matplotlib yacs lmdbdict scikit-image h5py tensorboardX nltk
 
